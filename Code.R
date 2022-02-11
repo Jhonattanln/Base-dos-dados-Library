338c557f2348e43443c2f015b8671cf9b7174d04
Jhonattan Lino <jhonattanlino@outlook.com>
2022-02-10 23:47:44 -0300

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,5 +1,11 @@
 ### Importando bibliotecas
+
 library(dplyr) ## biblioteca para manupulação de dados
 library(basedosdados) ## biblioteca para consuta dos dados
 library(tidyverse) 
 library(ggplot2) ## biblioteca para plotagem dos dados
+
+basedosdados::set_billing_id("resonant-petal-287222")
+# Para carregar o dado direto no R
+query <- "SELECT * FROM `basedosdados.br_anp_precos_combustiveis.microdados`"
+read_sql(query)
