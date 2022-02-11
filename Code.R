122cd557301f198e3cb53ff31262d5ba5c8125d0
Jhonattan Lino <jhonattanlino@outlook.com>
2022-02-10 22:31:10 -0300

    first

---
+++ Code.R
@@ -0,0 +1,4 @@
+### Importando bibliotecas
+library(dplyr) ## biblioteca para manupulação de dados
+library(basedosdados) ## biblioteca para consuta dos dados
+library(ggplot2) ## biblioteca para plotagem dos dados
