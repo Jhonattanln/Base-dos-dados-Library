fcbf54e91bff57074be340629e79c729245f9eb2
Jhonattan Lino <jhonattanlino@outlook.com>
2022-02-12 19:48:57 -0300

    Update Code.R

--- Code.R
+++ Code.R
@@ -7,7 +7,8 @@ library(ggplot2) ## biblioteca para plotagem dos dados
 
 basedosdados::set_billing_id("resonant-petal-287222")
 # Para carregar o dado direto no R
-query <- "SELECT * FROM `basedosdados.br_anp_precos_combustiveis.microdados`"
+query <- "SELECT * FROM `basedosdados.br_anp_precos_combustiveis.microdados` LIMIT 100"
 df <- read_sql(query)
 
 View(df)
+
